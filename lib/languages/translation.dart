eee6937153745508488c0d447dc0f2d451733b56
Linkia IT <info@linkia.qa>
2023-05-24 12:53:48 +0300

    fix

--- lib/languages/translation.dart
+++ lib/languages/translation.dart
@@ -2,13 +2,11 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 import 'ar.dart';
 import 'en.dart';
-import 'fr.dart';
 
 class Translation extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-    'ar': ar,
-    'en': en,
-    'fr': fr,
-  };
+        'ar': ar,
+        'en': en,
+      };
 }
